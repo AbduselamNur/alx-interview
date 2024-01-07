e9e7d9c6bc064c0e55ff208854baa1b02ccb5e0d
AbduselamNur <abduselamnurhussen626@gmail.com>
2024-01-07 21:51:52 +0300

    Add docstring to pascal_triangle function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,7 +2,11 @@
 """
 0-pascal_triangle
 """
+
 def pascal_triangle(n):
+    """
+    returns a list of lists of integers representing the Pascal’s triangle of n
+    """
     if n <= 0:
         return []
 
